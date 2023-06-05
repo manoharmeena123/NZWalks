1947b3a12dadc909b8292a2e0165ee777e9e487b
manoharmeena123 <manoharmeena245@gmail.com>
2023-06-05 18:01:13 +0530

    crud done

--- NZWalks/NZWalks.API/Models/DTO/Region.cs
+++ NZWalks/NZWalks.API/Models/DTO/Region.cs
@@ -10,5 +10,6 @@
         public double Lat { get; set; }
         public double Long { get; set; }
         public long Population { get; set; }
+        
     }
 }
--- NZWalks/NZWalks.API/Models/DTO/UpdateRegionRequest.cs
+++ NZWalks/NZWalks.API/Models/DTO/UpdateRegionRequest.cs
@@ -9,5 +9,6 @@
         public double Lat { get; set; }
         public double Long { get; set; }
         public long Population { get; set; }
+        
     }
 }
